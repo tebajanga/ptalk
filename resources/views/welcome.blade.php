7d41b1d347ed369bc8f2d6df8e5352c737b307e2
tebajanga <anthony.timothy90@gmail.com>
2017-04-23 05:31:38 +0300

    Adding signin and signup forms

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,41 @@
 @stop
 
 @section('content')
-    Welcome to People Talk
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Sign Up</h2>
+            <hr />
+            <form method="post" action="#">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" />
+                </div>
+                <div class="form-group">
+                    <label for="firstname">First Name</label>
+                    <input type="text" class="form-control" name="first_name" id="first_name" />
+                </div>
+                <div class="form-group">
+                    <label for="pasword">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" />
+                </div>
+                <button type="button" class="btn btn-primary">Sign Up</button>
+            </form>
+        </div><!-- close div .col-md-6 -->
+
+        <div class="col-md-6">
+            <h2>Sign In</h2>
+            <hr />
+            <form method="post" action="#">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" />
+                </div>
+                <div class="form-group">
+                    <label for="pasword">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" />
+                </div>
+                <button type="button" class="btn btn-primary">Sign In</button>
+            </form>
+        </div><!-- close div .col-md-6 -->
+    </div><!-- close div .row -->
 @stop
