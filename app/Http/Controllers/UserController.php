70eb44048ac0b8ffe219ec727d06d6ff2b279bae
tebajanga <anthony.timothy90@gmail.com>
2017-05-02 22:58:50 +0300

    Displaying posts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Post;
 
 class UserController extends Controller
 {
@@ -45,6 +46,7 @@ class UserController extends Controller
     }
 
     public function dashboard(){
-        return view('dashboard');
+        $posts = Post::all();
+        return view('dashboard',compact('posts'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,18 +24,20 @@
     <section class="row posts">
         <div class="col-md-6 col-md-offset-3">
             <header><h3>What other people says...</h3></header>
-            <article class="post">
-                <p>Lorem Ipsum</p>
-                <div class="info">
-                    Posted by Username on 01 May 2017
-                </div>
-                <div class="interaction">
-                    <a href="#">Like</a> | 
-                    <a href="#">Dislike</a> | 
-                    <a href="#">Edit</a> | 
-                    <a href="#">Delete</a>
-                </div>
-            </article>
+            @foreach($posts as $post)
+                <article class="post">
+                    <p>{{ $post->body }}</p>
+                    <div class="info">
+                        Posted by {{ $post->user->first_name }} on {{ $post->created_at }}
+                    </div>
+                    <div class="interaction">
+                        <a href="#">Like</a> | 
+                        <a href="#">Dislike</a> | 
+                        <a href="#">Edit</a> | 
+                        <a href="#">Delete</a>
+                    </div>
+                </article>
+            @endforeach
         </div>
     </section>
 @stop
